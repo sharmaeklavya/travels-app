b9180c823f23a46b5c6eca8af44117eb5f04d25e
eklavyasharma <24450132+sharmaeklavya@users.noreply.github.com>
2021-09-27 23:44:00 +0530

    flight section

--- src/comps/Flights.js
+++ src/comps/Flights.js
@@ -92,58 +92,57 @@ function Flights() {
               <button className="section-btn">Tour Packages</button>
             </div>
           </div>
-          <div className="row">
-            <div className="col-md-12">
-              <form className="flight-form">
-                <div className="form-row">
-                  <div className="col-12 col-sm-6 col-lg-3 mb-1">
-                    <label htmlFor="origin">From</label>
-                    <input
-                      id="origin"
-                      type="search"
-                      className="form-control"
-                      placeholder="Country, city origin"
-                    />
-                  </div>
-                  <div className="col-12 col-sm-6 col-lg-3 mb-1">
-                    <label htmlFor="destination">Going to</label>
-                    <input
-                      id="destination"
-                      type="search"
-                      className="form-control"
-                      placeholder="Country, city destination"
-                    />
-                  </div>
-                  <div className="col-12 col-sm-6 col-lg-3 mb-1">
-                    <label htmlFor="depart">Depart</label>
-                    <input
-                      id="depart"
-                      type="date"
-                      className="form-control"
-                      placeholder="Departure date"
-                    />
-                  </div>
-                  <div className="col-12 col-sm-6 col-lg-3 mb-1">
-                    <label htmlFor="return">Return</label>
-                    <input
-                      id="return"
-                      type="date"
-                      className="form-control"
-                      placeholder="Arrival date"
-                    />
-                  </div>
-                </div>
-                <div className="form-row mt-3">
-                  <div className="col text-center">
-                    <button type="button" className="flight-btn">
-                      Search flights
-                      <i className="fas fa-arrow-circle-right px-2"></i>
-                    </button>
-                  </div>
-                </div>
-              </form>
+          <form className="flight-form">
+            <div className="form-row">
+              <div className="col-12 mb-1">
+                <h1 className="flight-title">Fly business or leisure</h1>
+              </div>
+              <div className="col-12 col-sm-6 col-lg-3 mb-1">
+                <label htmlFor="origin">From</label>
+                <input
+                  id="origin"
+                  type="search"
+                  className="form-control"
+                  placeholder="Country, city origin"
+                />
+              </div>
+              <div className="col-12 col-sm-6 col-lg-3 mb-1">
+                <label htmlFor="destination">Going to</label>
+                <input
+                  id="destination"
+                  type="search"
+                  className="form-control"
+                  placeholder="Country, city destination"
+                />
+              </div>
+              <div className="col-12 col-sm-6 col-lg-3 mb-1">
+                <label htmlFor="depart">Depart</label>
+                <input
+                  id="depart"
+                  type="date"
+                  className="form-control"
+                  placeholder="Departure date"
+                />
+              </div>
+              <div className="col-12 col-sm-6 col-lg-3 mb-1">
+                <label htmlFor="return">Return</label>
+                <input
+                  id="return"
+                  type="date"
+                  className="form-control"
+                  placeholder="Arrival date"
+                />
+              </div>
             </div>
-          </div>
+            <div className="form-row mt-3">
+              <div className="col text-center">
+                <button type="button" className="flight-btn">
+                  Search flights
+                  <i className="fas fa-arrow-circle-right px-2"></i>
+                </button>
+              </div>
+            </div>
+          </form>
         </div>
       </section>
     </>
