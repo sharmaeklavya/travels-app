6d7228cd98f24c233806f64904a9aad8df26744a
eklavyasharma <24450132+sharmaeklavya@users.noreply.github.com>
2021-09-30 01:16:58 +0530

    nothing major

--- src/comps/Topbar.js
+++ src/comps/Topbar.js
@@ -4,6 +4,14 @@ import Currency from "./Currency";
 import axios from "axios";
 
 function Topbar() {
+  const hereMapToken =
+    "eyJhbGciOiJSUzUxMiIsImN0eSI6IkpXVCIsImlzcyI6IkhFUkUiLCJhaWQiOiIxTVdUT0lNRVNDYUhUWmk5ZlRvNiIsImlhdCI6MTYzMjk0MzE5NywiZXhwIjoxNjMzMDI5NTk3LCJraWQiOiJqMSJ9.ZXlKaGJHY2lPaUprYVhJaUxDSmxibU1pT2lKQk1qVTJRMEpETFVoVE5URXlJbjAuLjQ4WGR5MlVfV2JZNVJibE1wckJnb2cuNlVuTzdadmpGQ1lsM0pWVGk1VFYxa0xla0hEaFZydE9MUWhJLXZtQ29PUmVxa2xaOC0wMXotTU1QRmNrNnFET2FKVktkNkZKcXBjVExBaHJjaUZ5WVVXVkRvV05LcDIySDh5YzVNR1NlVkY0dmJxUjl6VEJzN2FlRDhNQ2xBUkZoenJXZ0V6a096dzNtNVMxZF9rOUxBLnQ4YXVEQ0tQRG1DTnFFd3FNUnpxZzZuU1J2SlhyaFZEZDA0ZmRBTGoyRDg.PkkYCHDrS75nToD2_y3r5G9EzO-7hFzcbpiYzgiB2T4BmxwZBKrCvFAZ0HZScQlyZ3HZOc9P7NX-FxG4Fgb-p2rMD8qhL5-dk3QI4_EXDS-un1nZ2jDgHDK4Ewfn3ikgpmmIDlCMU1JaTtSTGgINGNJoKQjD6cm5oUHxp2B-QowRSoHdwyFMI8ypaD4u1Ney_fDkaXZAvCJlTbSyY8OQHfyPlsTHFzuSqi1rvNWKKo7VuIZnl_s410Zqm7hEY_xknFK7FPF5LTZ4vK7kpPHqHZd8T8j6mIst_tEOGjtfVpglOGgb4s3OR0IcbQ_1FzkVuS2Sa5ybuSdh514ZBVw1Rw";
+
+  const [geoLocation, setGeoLocation] = useState({
+    latitude: "",
+    longitude: "",
+  });
+
   const [currency, setCurrency] = useState({
     name: localStorage.getItem("currencyName"),
     code: localStorage.getItem("currencyCode"),
@@ -19,8 +27,46 @@ function Topbar() {
   }, [currency]);
 
   useEffect(() => {
+    const success = (position) => {
+      setGeoLocation({
+        latitude: position.coords.latitude,
+        longitude: position.coords.longitude,
+      });
+    };
+
+    const error = (err) => {
+      console.error(`ERROR(${err.code}): ${err.message}`);
+    };
+
+    navigator.geolocation.getCurrentPosition(success, error, {
+      enableHighAccuracy: true,
+      timeout: 5000,
+      maximumAge: 0,
+    });
+  }, []);
+
+  // useEffect(() => {
+  //   if (geoLocation.latitude !== "") {
+  //     axios
+  //       .get(
+  //         `https://revgeocode.search.hereapi.com/v1/revgeocode?at=${geoLocation.latitude}%2C${geoLocation.longitude}&lang=en-US`,
+  //         {
+  //           headers: {
+  //             Authorization: `Bearer ${hereMapToken}`,
+  //           },
+  //           withCredentials: true,
+  //         }
+  //       )
+  //       .then((res) => console.log(res.data.items[0].address.countryName))
+  //       .catch((err) => console.log(err.response));
+  //   }
+  // }, [geoLocation]);
+
+  useEffect(() => {
     axios
-      .get("https://ipapi.co/json/")
+      .get("https://countryapi.gear.host/v1/Country/getCountries", {
+        withCredentials: true,
+      })
       .then((res) => console.log(res.data))
       .catch((res) => console.log(res.response));
   }, []);
